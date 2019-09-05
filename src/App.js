c574f08aadb0ddac793c9f53b0243fa7d977b51d
Jason Wong <jwong175@hotmail.com>
2019-09-05 16:55:39 -0700

    Render yelp search results

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './Map';
-
+import Home from './Home';
 
 class App extends Component {
 
@@ -10,32 +10,33 @@ class App extends Component {
     error: null,
   }
 
-  getYelp = () => {
+  // getYelp = () => {
 
-    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=los%20angeles&term=noodles`, {
-        headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
-        }
-    })
-    .then((res) => {
-        console.log(res.data.businesses)
-        this.setState({ business: res.data.businesses, loading: false })
-    })
-    .catch((err) => {
-        this.setState({ error: "Sorry, we couldn't find any shops around this area. Please enter another location and try again", loading: false})
-    })
-  }
+  //   axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=los%20angeles&term=noodles`, {
+  //       headers: {
+  //           Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+  //       }
+  //   })
+  //   .then((res) => {
+  //       console.log(res.data.businesses)
+  //       this.setState({ business: res.data.businesses, loading: false })
+  //   })
+  //   .catch((err) => {
+  //       this.setState({ error: "Sorry, we couldn't find any shops around this area. Please enter another location and try again", loading: false})
+  //   })
+  // }
 
-  
+
 
   render(){
     return (
       <div id="cover">
         <h1>NoodleMAPS</h1>
-        <hr/>
+        <br/>
         <Map/>
         <div className="content">
-          Something here
+          Noodles and stuff
+          <Home/>
         </div>
       </div>
     );
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import '../App.css';
+import axios from 'axios';
 
 class Home extends Component {
 
@@ -14,9 +15,28 @@ class Home extends Component {
       })
     }
     
-    // grab value and append it to the yelp search
-    onSubmit = () => {
-    
+    // grab search location and insert to yelp api
+    onSubmit = (event) => {
+      event.preventDefault();
+      console.log(this.state.search)
+
+      this.getYelp();
+    }
+
+    getYelp = () => {
+
+        axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${this.state.search}&term=noodles`, {
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+            }
+        })
+        .then((res) => {
+            console.log(res.data.businesses)
+            this.setState({ business: res.data.businesses, loading: false })
+        })
+        .catch((err) => {
+            this.setState({ error: "Sorry, we couldn't find any shops around this area. Please enter another location and try again", loading: false})
+        })
     }
 
     render(){
@@ -29,6 +49,7 @@ class Home extends Component {
                 placeholder="Enter Location"
                 onChange={this.onChange}
               />
+              <button type="submit">Go</button>
             </form>
         </div>
       )
